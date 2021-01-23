f9df540b03bc0b373ba2a26fe2037f321c262330
ethanbergstrom <ethan.bergstrom@gmail.com>
2021-01-23 12:06:28 -0600

    Testing of sysinternals broke the pipeline

--- Test/ChocolateyGet.tests.ps1
+++ Test/ChocolateyGet.tests.ps1
@@ -26,11 +26,12 @@ Describe "$platform basic package search operations" {
 		}
 	}
 	Context 'with additional arguments' {
-		$package = 'cpu-z'
-		$argsAndParams = '--exact'
+		$package = 'sysinternals'
+		$argsAndParams = '--paramsglobal --params "/InstallDir=c:\windows\temp\sysinternals /QuickLaunchShortcut=false" -y --installargs MaintenanceService=false'
+		$version = '2020.11.25'
 
 		It 'searches for the exact package name' {
-			Find-Package -Provider $ChocolateyGet -Name $package -AdditionalArguments $argsAndParams | Should Not BeNullOrEmpty
+			Find-Package -Provider $ChocolateyGet -Name $package -RequiredVersion $version -AdditionalArguments $argsAndParams | Should Not BeNullOrEmpty
 		}
 	}
 }
@@ -57,16 +58,16 @@ Describe "$platform DSC-compliant package installation and uninstallation" {
 		$argsAndParams = '--paramsglobal --params "/InstallDir=c:\windows\temp\sysinternals /QuickLaunchShortcut=false" -y --installargs MaintenanceService=false'
 
 		It 'searches for the latest version of a package' {
-			Find-Package -Provider $ChocolateyGet -Name $package -AdditionalArguments $argsAndParams | Where-Object {$_.Name -contains $package} | Should Not BeNullOrEmpty
+			Find-Package -Provider $ChocolateyGet -Name $package -RequiredVersion $version -AdditionalArguments $argsAndParams | Where-Object {$_.Name -contains $package} | Should Not BeNullOrEmpty
 		}
 		It 'silently installs the latest version of a package' {
-			Install-Package -Force -Provider $ChocolateyGet -Name $package -AdditionalArguments $argsAndParams | Where-Object {$_.Name -contains $package} | Should Not BeNullOrEmpty
+			Install-Package -Force -Provider $ChocolateyGet -Name $package -RequiredVersion $version -AdditionalArguments $argsAndParams | Where-Object {$_.Name -contains $package} | Should Not BeNullOrEmpty
 		}
 		It 'finds the locally installed package just installed' {
-			Get-Package -Provider $ChocolateyGet -Name $package -AdditionalArguments $argsAndParams | Where-Object {$_.Name -contains $package} | Should Not BeNullOrEmpty
+			Get-Package -Provider $ChocolateyGet -Name $package -RequiredVersion $version -AdditionalArguments $argsAndParams | Where-Object {$_.Name -contains $package} | Should Not BeNullOrEmpty
 		}
 		It 'silently uninstalls the locally installed package just installed' {
-			Uninstall-Package -Provider $ChocolateyGet -Name $package -AdditionalArguments $argsAndParams | Where-Object {$_.Name -contains $package} | Should Not BeNullOrEmpty
+			Uninstall-Package -Provider $ChocolateyGet -Name $package -RequiredVersion $version -AdditionalArguments $argsAndParams | Where-Object {$_.Name -contains $package} | Should Not BeNullOrEmpty
 		}
 	}
 }
@@ -87,11 +88,11 @@ Describe "$platform pipline-based package installation and uninstallation" {
 		$argsAndParams = '--paramsglobal --params "/InstallDir=c:\windows\temp\sysinternals /QuickLaunchShortcut=false" -y --installargs MaintenanceService=false'
 
 		It 'searches for and silently installs the latest version of a package' {
-			Find-Package -Provider $ChocolateyGet -Name $package | Install-Package -Force -AdditionalArguments $argsAndParams | Where-Object {$_.Name -contains $package} | Should Not BeNullOrEmpty
+			Find-Package -Provider $ChocolateyGet -RequiredVersion $version -Name $package | Install-Package -Force -AdditionalArguments $argsAndParams | Where-Object {$_.Name -contains $package} | Should Not BeNullOrEmpty
 		}
 
 		It 'finds and silently uninstalls the locally installed package just installed' {
-			Get-Package -Provider $ChocolateyGet -Name $package | Uninstall-Package -AdditionalArguments $argsAndParams | Where-Object {$_.Name -contains $package} | Should Not BeNullOrEmpty
+			Get-Package -Provider $ChocolateyGet -RequiredVersion $version -Name $package | Uninstall-Package -AdditionalArguments $argsAndParams | Where-Object {$_.Name -contains $package} | Should Not BeNullOrEmpty
 		}
 	}
 }
