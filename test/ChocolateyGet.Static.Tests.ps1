e09dd9fd2ec0a481f85df8ef3c9ef02f2eb06ad7
ethanbergstrom <ethan.bergstrom@gmail.com>
2021-07-05 11:39:56 -0500

    Fix static test formatting

--- test/ChocolateyGet.Static.Tests.ps1
+++ test/ChocolateyGet.Static.Tests.ps1
@@ -1,9 +1,9 @@
 Get-ChildItem .\ -Recurse | ForEach-Object {
-    Describe "File $($_) should not produce any PSScriptAnalyzer warnings" {
-        Invoke-ScriptAnalyzer -Path $_.FullName | ForEach-Object {
-            It "should not fail rule $($_.RuleName) on line $($_.Line) with message '$($_.Message)'" {
-                $false | Should -Be $true
-            }
-        }
-    }
+	Describe "File $($_) should not produce any PSScriptAnalyzer warnings" {
+		Invoke-ScriptAnalyzer -Path $_.FullName | ForEach-Object {
+			It "should not fail rule $($_.RuleName) on line $($_.Line) with message '$($_.Message)'" {
+				$false | Should -Be $true
+			}
+		}
+	}
 }
