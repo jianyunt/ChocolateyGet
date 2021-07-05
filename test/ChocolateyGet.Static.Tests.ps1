ef4954d47d7d1172d016a4a4e67cbfbb271d1178
ethanbergstrom <ethan.bergstrom@gmail.com>
2021-07-05 11:45:06 -0500

    Additional test refinement

--- test/ChocolateyGet.Static.Tests.ps1
+++ test/ChocolateyGet.Static.Tests.ps1
@@ -1,7 +1,7 @@
-Get-ChildItem .\ -Recurse | ForEach-Object {
-	Describe "File $($_) should not produce any PSScriptAnalyzer warnings" {
+Get-ChildItem .\ -Recurse -File | ForEach-Object {
+	Describe "file $($_) should not produce any PSScriptAnalyzer warnings" {
 		Invoke-ScriptAnalyzer -Path $_.FullName | ForEach-Object {
-			It "should not fail rule $($_.RuleName) on line $($_.Line) with message '$($_.Message)'" {
+			It "should not fail rule $($_.RuleName) on line $($_.Line) with message ""$($_.Message)""" {
 				$false | Should -Be $true
 			}
 		}
