9c892203152140d6038e40bb6fb0dea1948fd06f
ethanbergstrom <ethan.bergstrom@gmail.com>
2021-07-05 10:59:28 -0500

    Rework PSSA Pester tests

--- test/ChocolateyGet.Static.Tests.ps1
+++ test/ChocolateyGet.Static.Tests.ps1
@@ -1,11 +1,8 @@
-$scriptAnalyzerRules = Get-ScriptAnalyzerRule
-
 Get-ChildItem .\ -Recurse -Filter *.ps*1 | ForEach-Object {
     Describe "File $($_) should not produce any PSScriptAnalyzer warnings" {
-        $analysis = Invoke-ScriptAnalyzer -Path $_.FullName
-        foreach ($rule in $scriptAnalyzerRules) {
-            It "Should pass $rule" {
-                $analysis | Where-Object RuleName -EQ $rule | Should -BeNullOrEmpty
+        Invoke-ScriptAnalyzer -Path $_.FullName | ForEach-Object {
+            It "should not fail rule $($_.RuleName) on line $($_.Line) with message '$($_.Message)'" {
+                $false | Should -Be $true
             }
         }
     }
