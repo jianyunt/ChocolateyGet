91e771b77875aeae88919e93222a02dd69f37685
Ethan Bergstrom <13603162+ethanbergstrom@users.noreply.github.com>
2023-06-03 23:15:05 +0000

    Accounting for choco extension uninstalls

--- test/ChocolateyGet.chocoV2.Unit.Tests.ps1
+++ test/ChocolateyGet.chocoV2.Unit.Tests.ps1
@@ -140,7 +140,7 @@ Describe 'pipeline-based package installation and uninstallation' {
 			Find-Package -Provider 'ChocolateyGet' -Name $package | Install-Package -Force | Where-Object {$_.Name -contains $package} | Should -Not -BeNullOrEmpty
 		}
 		It 'finds and silently uninstalls the locally installed package just installed, along with its dependencies' {
-			Get-Package -Provider 'ChocolateyGet' -Name $package | Uninstall-Package -RemoveDependencies | Should -HaveCount 2
+			Get-Package -Provider 'ChocolateyGet' -Name $package | Uninstall-Package -RemoveDependencies | Should -HaveCount 4
 		}
 	}
 	Context 'with additional parameters' {
